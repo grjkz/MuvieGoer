72ec4ccb080070bab3ae5be39edd574db657d369
Philip Choi <gorejakz@gmail.com>
2016-04-24 22:41:23 -0400

    now shows last 10 reviews on welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+  	@reviews = Review.order('created_at DESC').limit(10)
   	@review = Review.last
+  	@movie = @review.movie
   end
 end
