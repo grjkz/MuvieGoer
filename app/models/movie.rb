9a84cccaa3bbe68fea487c96d563189654ce2ddc
Philip Choi <gorejakz@gmail.com>
2016-04-25 15:42:27 -0400

    changed how original imgage file is modified

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,19 +4,23 @@ class Movie < ActiveRecord::Base
 	# picture
 	# has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	 #  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	has_attached_file :image, styles: { small: "110x170", med: "180x240", large: "240x360" }, default_url: 'http://placehold.it/180x240'
+
+	# style: original will overwrite the actual uploaded image. useful for gigantic image files
+	has_attached_file :image, styles: { small: "110x170", med: "180x240", original: '240x360' }, default_url: 'http://placehold.it/180x240'
 	validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"] }
 
 	has_many :reviews
 
 
-	after_save :destroyOriginal
+	# after_save :destroyOriginal
 
 	private
 
 	# destroys the original size uploaded image to save space
 	# you can also just set the style, 'original' to whatever you want and it should replace the uploaded one
-	def destroyOriginal
-		File.unlink(self.image.path) # self.<column name>.path
-	end
+	# def destroyOriginal
+	# 	File.unlink(self.image.url) 
+	# 	# self.<column name>.path # gives absolute path from computer root
+	# 	# self.<column name>.url # gives absolute path from app root
+	# end
 end
