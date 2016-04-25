026e7d2eb3fe0333b3cab52cd0fb240c831768a8
Philip Choi <gorejakz@gmail.com>
2016-04-24 21:30:34 -0400

    users can now post reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,14 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    
+    @review = Review.create(review_params)
+    @movie = Movie.find(params[:movie_id])
+    @user = User.find(session[:user_id])
+    @review.movie_id = @movie.id 
+    @review.user_id = @user.id 
+    @review.save
+
+    redirect_to @movie
   end
 
   def show
@@ -22,5 +29,12 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
   end
 end
