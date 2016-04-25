7bc4fabbba84361bb671e401dbfc9c8e6ac1b28b
Philip Choi <gorejakz@gmail.com>
2016-04-25 19:42:48 -0400

    styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   	@reviews = Review.order('created_at DESC').limit(10)
-  	@newestMovies = Movie.last(3)
+  	@newestMovies = Movie.order('id DESC').limit(3)
   end
 end
