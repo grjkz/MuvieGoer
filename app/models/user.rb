9a8da04d684d5f6101ac4c1d003c97b92df8516f
Philip Choi <gorejakz@gmail.com>
2016-04-25 23:46:03 -0400

    changed uniqueness to case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :alias, presence: true, uniqueness: true, length: {minimum: 6, maximum: 20}
-  validates :email, presence: true, uniqueness: true
+  validates :alias, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 6, maximum: 20}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: {minimum: 4}, confirmation: true
   validates :password_confirmation, presence: true
 
