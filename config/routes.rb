e9fa6a022df91ef57cb4d627eb413b1b02ca68fc
Philip Choi <gorejakz@gmail.com>
2016-04-25 23:52:54 -0400

    restricted routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :movies do
+  resources :movies, only: [:index] do
     resources :reviews, except: [:show]
   end
   
@@ -24,8 +24,8 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   get '/signout' => 'sessions#destroy'
   
-  resources :sessions
-  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 
   get "*path" => redirect('/')
   
