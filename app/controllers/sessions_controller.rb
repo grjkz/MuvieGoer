b4347e3b702ceac65a0e97dee84ea1b033881d5b
Philip Choi <gorejakz@gmail.com>
2016-04-26 13:34:18 -0400

    fixed error message type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   		session[:user_id] = @user.id
   		redirect_to root_path
   	else
-  		flash[:error] = "Invalid Username/Password"
+  		flash[:error] = ["Invalid Username/Password"]
   		redirect_to signin_path
   	end
   end
