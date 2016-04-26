f757ee42f749cf5da0053f10afd1895cb692a8a7
Philip Choi <gorejakz@gmail.com>
2016-04-25 23:15:29 -0400

    email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :alias, presence: true, uniqueness: true, length: {minimum: 6, maximum: 20}
-  validates :email, presence: true#, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, length: {minimum: 4}, confirmation: true
   validates :password_confirmation, presence: true
 
