b5735cfff5299a6daf58a6103c0a55576983f19d
Philip Choi <gorejakz@gmail.com>
2016-04-27 14:12:24 -0400

    reduced static star sizes and rating text; increased stars to show 10; added conditional for rating initializer

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -2,9 +2,11 @@ $(function() {
 	// enable static star ratings for movie details
 	$('.static-rating').each(function(index, el) {
 		$(el).rateYo({
+			starWidth: "12px",
+			numStars: 10,
 			maxValue: 10,
 			precision: 1,
-			rating: this.getAttribute('data-value'),
+			rating: this.getAttribute('data-value') === "-" ? 0 : this.getAttribute('data-value'),
 			readOnly: true
 		});
 	});
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,9 +1,10 @@
 $(function() {
 	// enable star ratings for review#new and review#edit
 	$('#rateyo').rateYo({
+		numStars: 10,
 		maxValue: 10,
 		rating: $('#review_rating').val(),
-		halfStar: true,
+		fullStar: true,
 		onSet: function(rating, rateYoInstance) {
 			$('#review_rating').val(rating);
 		}
