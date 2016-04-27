484ce8d1bcad69e46e0fd39995ac582e48b50f61
Philip Choi <gorejakz@gmail.com>
2016-04-27 00:25:13 -0400

    added custom error message

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
 
-  validates_inclusion_of :rating, in: 0..10
+  validates_inclusion_of :rating, in: 0..10, :message => "is invalid! Don't try to break the rating system!"
 end
