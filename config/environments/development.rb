a95ccc595441c7d5d9a7c6dc6097cafa4fd3d0fd
Philip Choi <gorejakz@gmail.com>
2016-04-28 17:17:56 -0400

    working upload to s3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,14 +12,16 @@ Rails.application.configure do
   }
 
 
-  # config.paperclip_defaults = {
-  #   :storage => :s3,
-  #   :s3_credentials => {
-  #     :bucket => ENV['S3_BUCKET_NAME'],
-  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #   }
-  # }
+  # store image uploads on s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :path => "/muviegoer/development/:class/:attachment/:id_partition/:style/:filename",
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
