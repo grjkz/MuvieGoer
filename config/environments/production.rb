43a52f66daf6a029160d8178c202206ac0175aa7
Philip Choi <gorejakz@gmail.com>
2016-04-28 17:18:06 -0400

    working upload to s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,14 +13,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "muviegoer.herokuapp.com" }
 
-  # config.paperclip_defaults = {
-  #   :storage => :s3,
-  #   :s3_credentials => {
-  #     :bucket => ENV['S3_BUCKET_NAME'],
-  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #   }
-  # }
+
+  # store image uploads on s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :path => "/muviegoer/:class/:attachment/:id_partition/:style/:filename",
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
 
   # Settings specified here will take precedence over those in config/application.rb.
